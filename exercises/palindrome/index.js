8d9cf2d2d47892ec83c8684cd26b8e786ffa9742
Sato Ryoka <satoryoka0920@gmail.com>
2021-08-04 18:52:23 +0900

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -9,7 +9,8 @@
 
 function palindrome(str) {
   const splitedString = str.split("")
-  //俺の回答
+
+  //俺の解答
   //strの文字数を測定
   //仮にstrの文字数を11としてループを回す。
   //strの最初の文字と最後の文字を比較
@@ -17,11 +18,20 @@ function palindrome(str) {
   //奇数の場合真ん中の文字は無視
   //すべての回でtrueであればtrueを返す
   //いずれかでfalseの場合はfalseを返す
-  for (let i = 0; i < splitedString.length; i++) {
-    return splitedString[i] == splitedString[splitedString.length - 1 - i]
-  }
+  // for (let i = 0; i < splitedString.length; i++) {
+  //   return splitedString[i] == splitedString[splitedString.length - 1 - i]
+  // }
+
+  //Sotution2 everyを使った方法
+  return splitedString.every((char, idx) => {
+    return char === str[str.length - idx - 1]
+  })
+
+  //Solution1 文字ひっくり返して元の変数と一致するか判定
+  // const reversed = str.split("").reverse().join("")
+  // return str === reversed
 }
 
-console.log(palindrome("aaaiiiaaa"))
+console.log(palindrome("aiai"))
 
 module.exports = palindrome
---
+++ exercises/palindrome/index2.js
@@ -0,0 +1,5 @@
+function palindrome(str) {
+  //Solution1 文字ひっくり返して元の変数と一致するか判定
+  //Solution2 everyを使った方法
+  //番外編 俺の解答
+}
